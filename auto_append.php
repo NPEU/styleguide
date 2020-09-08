7a30e7966f5a8120f2dbace67fe0b798d713cf01
Andy Kirk <andy.kirk@npeu.ox.ac.uk>
2020-09-08 09:46:54 +0100

    WiP

--- auto_append.php
+++ auto_append.php
@@ -2,13 +2,14 @@
 $output = ob_get_contents();
 ob_end_clean();
 
-#$output = str_replace('http://localhost:4001/', 'https://lab.npeu.ox.ac.uk/mockup/', $output);
-$output = str_replace('href="/',        'href="/mockup/', $output);
-$output = str_replace('<script src="/', '<script src="/mockup/', $output);
-$output = str_replace('/downloads',     '/mockup/downloads', $output);
-$output = str_replace('/img',           '/mockup/img', $output);
-$output = str_replace('data="/',        'data="/mockup/', $output);
-$output = str_replace('name="msapplication-config" content="/', 'name="msapplication-config" content="/mockup/', $output);
+#$output = str_replace('http://localhost:4001/', 'https://lab.npeu.ox.ac.uk/styleguide/', $output);
+$output = str_replace('href="/',        'href="/styleguide/', $output);
+$output = str_replace('link.href = \'/','link.href = \'/styleguide/', $output);
+$output = str_replace('<script src="/', '<script src="/styleguide/', $output);
+$output = str_replace('/downloads',     '/styleguide/downloads', $output);
+$output = str_replace('/img',           '/styleguide/img', $output);
+$output = str_replace('data="/',        'data="/styleguide/', $output);
+$output = str_replace('name="msapplication-config" content="/', 'name="msapplication-config" content="/styleguide/', $output);
 
 echo $output;
 ?>
